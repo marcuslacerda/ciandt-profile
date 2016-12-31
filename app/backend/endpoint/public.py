bf608ed8f18dfe32c1df26b3cd026347585ac8d8
Marcus Lacerda <marcus.lacerda@gmail.com>
2016-12-31 08:19:58 -0200

    incremente version controle for each build

--- app/backend/endpoint/public.py
+++ app/backend/endpoint/public.py
@@ -1,10 +1,6 @@
 from backend import app
 from backend.version import __version__
-
 from flask import jsonify
-import logging
-
-logger = logging.getLogger('stack')
 
 @app.route('/api/public/version', methods = ['GET'])
 def api_version():
--- app/backend/version.py
+++ app/backend/version.py
@@ -1,11 +1,2 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# This file is part of sandbox.
-# https://github.com/marcuslacerda/profile
-
-# Licensed under the MIT license:
-# http://www.opensource.org/licenses/MIT-license
-# Copyright (c) 2016, Marcus Lacerda <marcus.lacerda@gmail.com>
-
-__version__ = '0.0.1'  # NOQA
+"""Version Control."""
+__version__ = '0.0.BUILD_NUMBER'
