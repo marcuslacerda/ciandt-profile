5349f537955f47c0a86e560c04c4b92e73bb5e49
Marcus Lacerda <marcus.lacerda@gmail.com>
2017-06-07 16:54:53 -0300

    fix job scripts for people

--- jobs/script_load_profile.py
+++ jobs/script_load_profile.py
@@ -32,7 +32,7 @@ def load_people():
     for hit in people.find_all_users():
         count += 1
         logger.info("Loading %s - %s  " % (hit['login'], count))
-        # project = people.find_project_by_user(hit['login'])
+        project = people.find_project_by_user(hit['login'])
 
         if hit['admission']:
             admission = datetime.utcfromtimestamp(hit['admission'] / 1000)
@@ -49,10 +49,10 @@ def load_people():
                'admission': admission,
                'admissionReal': admission_real,
                'birthday': birthday,
-            #    'project': {
-            #         'code': hit['project']['code'],
-            #         'name': project
-            #    },
+               'project': {
+                    'code': hit['project']['code'],
+                    'name': project
+               },
                'area': hit['area'],
                'company': hit['company'],
                'status': hit['status']
--- jobs/script_update_rescission.py
+++ jobs/script_update_rescission.py
@@ -39,7 +39,10 @@ def update_rescission():
         (status_code,  people_doc) = people.find_user(hit['login'])
 
         if status_code == 200 and people_doc['status'] != 'A':
-            rescission = datetime.utcfromtimestamp(people_doc['rescission'] / 1000)
+            if people_doc.get('rescission'):
+                rescission = datetime.utcfromtimestamp(people_doc['rescission'] / 1000)
+            else:
+                rescission = None
             doc_status = {
                 "doc": {
                       "rescission": rescission,
